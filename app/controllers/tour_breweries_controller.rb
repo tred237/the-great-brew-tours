d96cf2f21f8030206bd608c5fd3cb9eaefd4a279
trevor dewindt <trevor.dewindt@gmail.com>
2023-06-27 22:17:15 -0600

    add tour breweries create controller

--- app/controllers/tour_breweries_controller.rb
+++ app/controllers/tour_breweries_controller.rb
@@ -1,2 +1,21 @@
+require 'byebug'
 class TourBreweriesController < ApplicationController
+    def create
+        # change to session id
+        creator = User.find(params[:creator_id])
+        if creator.is_admin
+            breweries = []
+            current_tour_breweries = TourBrewery.where("tour_id = #{params[:tour_id]}").map{|t| t.brewery_id}
+            params_breweries = params[:breweries].filter{ |b| !current_tour_breweries.include? b}
+            if params_breweries.length > 0
+                params_breweries.map do |b|
+                    brewery = TourBrewery.create!(tour_id: params[:tour_id], brewery_id: b)
+                    breweries.push brewery
+                end
+            end
+            render json: breweries, status: :created
+        else
+            render json: {errors: ["You are not authorized to add a brewery to a tour"]}, status: :unauthorized
+        end
+    end
 end
