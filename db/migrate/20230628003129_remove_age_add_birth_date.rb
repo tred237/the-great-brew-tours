be578372f5060d8247b094769e218f8e60e46dad
trevor dewindt <trevor.dewindt@gmail.com>
2023-06-29 18:07:30 -0600

    migration fix attempt

--- db/migrate/20230628003129_remove_age_add_birth_date.rb
+++ db/migrate/20230628003129_remove_age_add_birth_date.rb
@@ -2,5 +2,6 @@ class RemoveAgeAddBirthDate < ActiveRecord::Migration[7.0]
   def change
     remove_column :users, :age, :float
     add_column :users, :birth_date, :date
+    change_column_null :users, :birth_date, false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -77,7 +77,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_28_020107) do
     t.boolean "is_admin", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "birth_date"
+    t.date "birth_date", null: false
   end
 
   add_foreign_key "breweries", "users", column: "creator_id"
