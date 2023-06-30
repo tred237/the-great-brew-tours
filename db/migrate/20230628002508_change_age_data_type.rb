496b1f0101fff580c713f2a84097ef537a5fa193
trevor dewindt <trevor.dewindt@gmail.com>
2023-06-29 20:30:41 -0600

    revert migration to reversible syntax

--- db/migrate/20230628002508_change_age_data_type.rb
+++ db/migrate/20230628002508_change_age_data_type.rb
@@ -1,13 +1,10 @@
 class ChangeAgeDataType < ActiveRecord::Migration[7.0]
-  def up
-    change_table :users do |t|
-      t.change :age, :float
-    end
-  end
-
-  def down
-    change_table :users do |t|
-      t.change :age, :integer
+  def change
+    reversible do |direction|
+      change_table :users do |t|
+        direction.up { t.change :age, :float }
+        direction.down { t.change :age, :integer }
+      end
     end
   end
 end
