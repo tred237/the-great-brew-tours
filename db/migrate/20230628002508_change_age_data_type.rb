edc514288e450669ca046f0cb613e2b6f4e74d9e
trevor dewindt <trevor.dewindt@gmail.com>
2023-06-29 19:11:00 -0600

    migration up down test

--- db/migrate/20230628002508_change_age_data_type.rb
+++ db/migrate/20230628002508_change_age_data_type.rb
@@ -1,10 +1,13 @@
 class ChangeAgeDataType < ActiveRecord::Migration[7.0]
-  def change
-    reversible do |direction|
-      change_table :users do |t|
-        direction.up { t.change :age, :float }
-        direction.down { t.change :age, :integer }
-      end
+  def up
+    change_table :users do |t|
+      t.change :age, :float
+    end
+  end
+
+  def down
+    change_table :users do |t|
+      t.change :age, :integer
     end
   end
 end
