4455656b6cf9e7b349d24cb56b09e090a88adf3f
trevor dewindt <trevor.dewindt@gmail.com>
2023-07-03 16:23:18 -0600

    add scheduled tours controllers

--- app/controllers/scheduled_tours_controller.rb
+++ app/controllers/scheduled_tours_controller.rb
@@ -9,9 +9,33 @@ class ScheduledToursController < ApplicationController
         render json: scheduled_tour, status: :created
     end
 
+    def index
+        user = find_user
+        scheduled_tours = user.scheduled_tours.all
+        render json: scheduled_tours, status: :ok
+    end
+
+    def update
+        updater = find_user
+        scheduled_tour = updater.scheduled_tours.find(params[:id])
+        scheduled_tour.update!(update_scheduled_tours_params)
+        render json: scheduled_tour, status: :ok
+    end
+
+    def destroy
+        destroyer = find_user
+        scheduled_tour = destroyer.scheduled_tours.find(params[:id])
+        scheduled_tour.destroy
+        head :no_content
+    end
+
     private
 
     def scheduled_tours_params
         params.permit(:tour_id, :number_of_people)
+    end
+
+    def update_scheduled_tours_params
+        params.permit(:number_of_people)
     end
 end
