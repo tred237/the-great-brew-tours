5c29f64d66e156ed1e6b7691896f5004559310af
trevor dewindt <trevor.dewindt@gmail.com>
2023-07-04 16:06:28 -0600

    fix breweries controller auth bug and add username to brewery review serializer

--- app/serializers/brewery_review_serializer.rb
+++ app/serializers/brewery_review_serializer.rb
@@ -1,3 +1,7 @@
 class BreweryReviewSerializer < ActiveModel::Serializer
-  attributes :id, :is_recommended, :review, :is_edited, :created_at
+  attributes :id, :is_recommended, :review, :is_edited, :created_at, :user_id, :review_username
+
+  def review_username
+    User.find(self.object.user_id).username
+  end
 end
