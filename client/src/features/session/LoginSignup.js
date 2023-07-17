9e67d0e138e47d46e26bd81a8debaf44015ed5b8
trevor dewindt <trevor.dewindt@gmail.com>
2023-07-17 16:51:09 -0600

    login signup modal title change

--- client/src/features/session/LoginSignup.js
+++ client/src/features/session/LoginSignup.js
@@ -5,14 +5,16 @@ import Button from 'react-bootstrap/esm/Button';
 import LoginForm from "./LoginForm";
 import SignupForm from "./SignupForm";
 
-export default function LoginSignup({ onCloseModal }) {
+export default function LoginSignup({ onCloseModal, changeTitle }) {
     const [isSignup, setIsSignup] = useState(false)
 
-    const handleClick = () => setIsSignup(!isSignup)
+    const handleClick = () => {
+        setIsSignup(!isSignup)
+        changeTitle()
+    }
 
     return (
         <Container>
-            <h3>{isSignup ? 'Create Account' : 'Login'}</h3>
             {isSignup ? <SignupForm onCloseModal={onCloseModal} isLogin={handleClick} /> : <LoginForm onCloseModal={onCloseModal} />}
             <Button variant="link" onClick={handleClick}>{isSignup ? 'Log In' : 'Create an Account'}</Button>
         </Container>
--- client/src/modals/LoginSignupModal.js
+++ client/src/modals/LoginSignupModal.js
@@ -1,14 +1,19 @@
+import { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import LoginSignup from '../features/session/LoginSignup';
 
 export default function LoginSignupModal({ showModal, onCloseModal }) {
+    const [isSignupTitle, setIsSignupTitle] = useState(false)
+
+    const changeTitle = () => setIsSignupTitle(!isSignupTitle)
+
     return (
         <Modal show={showModal} onHide={onCloseModal} animation={false}>
             <Modal.Header closeButton>
-                {/* <Modal.Title>Write a review</Modal.Title> */}
+                <Modal.Title>{isSignupTitle ? 'Create An Account' : 'Log In'}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <LoginSignup onCloseModal={onCloseModal} />
+                <LoginSignup onCloseModal={onCloseModal} changeTitle={changeTitle} />
             </Modal.Body>
         </Modal>
     )
