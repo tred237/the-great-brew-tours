8ac6043b1cdb887e7bfb451d082626fa39cc729a
trevor dewindt <trevor.dewindt@gmail.com>
2023-07-21 22:52:46 -0500

    replace duplicated logic in time helper

--- client/src/helpers/time.js
+++ client/src/helpers/time.js
@@ -1,17 +1,17 @@
-const withSeconds = (time, hour, minutes, seconds, newHour) => {
+const withSeconds = (hour, minutes, seconds, newHour) => {
     if(newHour) return `${newHour}:${minutes}:${seconds} PM MST`
     else if(hour === '12') return `${hour}:${minutes}:${seconds} PM MST`
     else if(['10','11'].includes(hour)) return `${hour}:${minutes}:${seconds} AM MST`
     else if(hour === '00') return `12:${minutes}:${seconds} AM MST`
-    else return `${hour.split('')[1]}:${time.split(':')[1]}:${seconds} AM MST`
+    else return `${hour.split('')[1]}:${minutes}:${seconds} AM MST`
 }
 
-const withOutSeconds = (time, hour, minutes, newHour) => {
+const withOutSeconds = (hour, minutes, newHour) => {
     if(newHour) return `${newHour}:${minutes} PM MST`
     else if(hour === '12') return `${hour}:${minutes} PM MST`
     else if(['10','11'].includes(hour)) return `${hour}:${minutes} AM MST`
     else if(hour === '00') return `12:${minutes} AM MST`
-    else return `${hour.split('')[1]}:${time.split(':')[1]} AM MST`
+    else return `${hour.split('')[1]}:${minutes} AM MST`
 }
 
 export default function formatTime(time, seconds = false) {
