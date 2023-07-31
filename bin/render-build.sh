edab0e2ad7f5298d3145344bef74022570000a2c
trevor dewindt <trevor.dewindt@gmail.com>
2023-07-31 00:51:04 -0600

    remove db:reset test

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,8 +9,8 @@ cp -a client/build/. public/
 
 # Build commands for back end
 bundle install
-bundle exec rake db:drop
-bundle exec rake db:create
+bundle exec rake db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+bundle exec rake db:create DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bundle exec rake db:migrate 
 # change back to seed!
 bundle exec rake db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1 # if you have seed data, run this command for the initial deploy only to avoid duplicate records
