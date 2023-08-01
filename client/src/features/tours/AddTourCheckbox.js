c7ad0b6db638fcf85944aa87d7e1fac333534052
trevor dewindt <trevor.dewindt@gmail.com>
2023-07-31 22:00:53 -0600

    add sorting to tour brewery checkbox list

--- client/src/features/tours/AddTourCheckbox.js
+++ client/src/features/tours/AddTourCheckbox.js
@@ -2,6 +2,8 @@ import { useSelector } from 'react-redux';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/esm/Row';
 
+import { sortAscending } from '../../helpers/sort';
+
 export default function AddTourCheckbox({ handleChange }) {
     const breweries = useSelector(state => state.breweries.breweries)
     const tourErrors = useSelector(state => state.tours.addTourErrors)
@@ -11,7 +13,7 @@ export default function AddTourCheckbox({ handleChange }) {
             <Form.Label>Breweries *</Form.Label>
             {tourErrors && tourErrors.tour_breweries ? tourErrors.tour_breweries.map(e => <p className="error-message" key={e}>{e}</p>) : null}
             <Row md={2}>
-                {breweries.map(b => {
+                {breweries.slice().sort((a,b) => sortAscending(a.name.toLowerCase(), b.name.toLowerCase())).map(b => {
                     return (<Form.Check key={b.id}
                     type='checkbox'
                     name='breweries'
