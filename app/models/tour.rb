f7d7f619d964cd6dcfd4e48bf2c6e3e9096dd940
trevor dewindt <trevor.dewindt@gmail.com>
2023-07-31 20:53:56 -0600

    time zone change in tour date validation

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -12,8 +12,18 @@ class Tour < ApplicationRecord
     validate :tour_date_after_current_date
 
     def tour_date_after_current_date
-        # byebug
-        unless tour_date and tour_date.to_date > Date.today.in_time_zone('US/Mountain').to_date #Date.today
+        byebug
+        current_date = DateTime.now.in_time_zone('US/Mountain')
+        current_year = current_date.year.to_s
+        current_month = current_date.month < 10 ? "0#{current_date.month.to_s}" : current_date.month.to_s
+        current_day = current_date.day < 10 ? "0#{current_date.day.to_s}" : current_date.day.to_s
+
+        tour_year = tour_date.year.to_s
+        tour_month = tour_date.month < 10 ? "0#{tour_date.month.to_s}" : tour_date.month.to_s
+        tour_day = tour_date.day < 10 ? "0#{tour_date.day.to_s}" : tour_date.day.to_s
+
+        unless tour_date and (tour_year + tour_month + tour_day).to_i > (current_year + current_month + current_day).to_i
+        # unless tour_date and tour_date.to_date > Date.today.in_time_zone('US/Mountain').to_date #Date.today
             errors.add(:tour_date, "must occur after current date")
         end
     end
